569ef37c96a5c1309759e7f508203cb7e42f4d7e
ruwan <45613229+RuwanRanaweera@users.noreply.github.com>
2021-03-30 06:22:55 +0530

    no commit

---
+++ 5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void swap (int *a, int *b);
+int main(){
+	
+int m=22,n=44;
+	
+	printf("m=%d and n=%d\n",m,n);
+	swap(&m,&n);
+	
+	
+}
+
+void swap(int *a, int *b){
+	
+	int tmp;
+	tmp=*a;
+	*a=*b;
+	*b=tmp;
+	
+	printf("a=%d and b=%d",*a,*b);
+	
+	
+}
+
